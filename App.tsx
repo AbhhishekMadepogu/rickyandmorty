5bfd7db111247d8c96f552b8ee0cc03368932ccf
Abhishek Madepogu <abhishek.madepogu@gmail.com>
2023-02-21 21:49:37 -0500

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import {SafeAreaView, Text} from 'react-native';
+import React from 'react';
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+}
